ba1b7e12b1cbea286c3c333eeb1d62a2cb526448
neelkanani <kananineel97@gmail.com>
2025-07-07 14:54:48 +0530

    test

---
+++ leak.js
@@ -0,0 +1,9 @@
+// leak-example.js
+
+const OPEN_API_KEY = "sk-1234567890abcdefghijklmnopqrst"; // Fake OpenAI API Key
+
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+const PASSWORD = "SuperSecret123!";
+
+const GITHUB_TOKEN = "ghp_abcd1234efgh5678ijkl9012mnopqrstu";
